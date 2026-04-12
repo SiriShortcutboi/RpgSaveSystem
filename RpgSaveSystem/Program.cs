32f9c7d77785ec2faa1e1d00a50ccf207307cf95
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-04-12 16:02:38 -0600

    progress in main

--- RpgSaveSystem/Program.cs
+++ RpgSaveSystem/Program.cs
@@ -1,6 +1,41 @@
 ﻿public class Program
 {
+   
 	public static void Main()
 	{
-	}
+        string inputLoadUserSave = "0";
+
+        Console.WriteLine("Welcome to the Squid Wizard RPG");
+        Console.WriteLine("Press 1 or Enter to continue story (load save data)");
+        Console.WriteLine("Press 2 to load new game");
+        inputLoadUserSave = Console.ReadLine(); 
+
+        string inputLoadUserSave = Console.ReadLine() ?? string.Empty;
+        inputLoadUserSave = inputLoadUserSave.ToLower().Trim();
+
+        if (string.IsNullOrWhiteSpace(inputLoadUserSave) || inputLoadUserSave == "1" || inputLoadUserSave == "enter")
+        {
+            return System.Text.Json.JsonSerializer.Deserialize(Player); //the save data;
+        }
+
+        else if (inputLoadUserSave == "2")
+        {
+            Player player = new Player();
+        }
+        else 
+        {
+            Console.WriteLine("Selection not understood. Loading save data");
+            return System.Text.Json.JsonSerializer.Deserialize(Player); 
+        }
+        
+        if (inputLoadUserSave == "2")
+        {
+            System.Console.WriteLine("Welcome new Player!");
+        }
+
+        Console.WriteLine("Your current player stats are:");
+        Console.WriteLine(Player);
+
+    }
+
 }
