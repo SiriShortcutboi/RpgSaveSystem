2bd9904dbce6ef31b79aa20bdd1bc737847acd3c
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-04-19 22:26:12 -0600

    added list to hold the palyer attributes, learned about AsReadOnly

--- RpgSaveSystem/Player.cs
+++ RpgSaveSystem/Player.cs
@@ -1,12 +1,16 @@
 using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 namespace PlayerObjectSpace;
 public class Player : IGameCharacter, ISaveable
 {
-    private int Name { get; set; }
-	private int Health{ get; set; }
-	private int Level;
-	private Player(string name, int health, int level)
+	private readonly List<Item> _inventory = new List<Item>();
+	public IReadOnlyList<Item> Inventory => _inventory.AsReadOnly();
+
+	public string Name { get; private set; }
+	public int Health{ get; private set; }
+	public int Level { get; private set; }
+	public Player(string name, int health, int level)
 	{
 		this.Name = name;
 		this.Health = health;
@@ -15,18 +19,18 @@ public class Player : IGameCharacter, ISaveable
 	}
 	public void AddItem(Item item)
 	{
-
+		_inventory.Add(item);
 	}
 
 	public void TakeDamage(int amount)
 	{
-        health = health - amount;
-        if (health >= 0)
-        {
-            health = 0;
-        }
-        if (heatlh == 0)
-            Console.WriteLine("You have fainted.");
+		Health = Health - amount;
+		if (Health < 0)
+		{
+			Health = 0;
+		}
+		if (Health == 0)
+			Console.WriteLine("You have fainted.");
 
          
 
